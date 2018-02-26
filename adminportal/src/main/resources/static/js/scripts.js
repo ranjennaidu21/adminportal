c8d7e8362b2e723ca7113e3ccce94b733111f2ea
Anonymous <Anonymous@Anonymous-PC>
2018-02-26 17:44:34 +0800

    Adminportal : Add Delete Book Functionality

--- adminportal/src/main/resources/static/js/scripts.js
+++ adminportal/src/main/resources/static/js/scripts.js
@@ -2,39 +2,80 @@
  * 
  */
 
-function checkBillingAddress() {
-	if($("#theSameAsShippingAddress").is(":checked")) {
-		$(".billingAddress").prop("disabled", true);
-	} else {
-		$(".billingAddress").prop("disabled", false);
-	}
-}
-
-function checkPasswordMatch() {
-	var password = $("#txtNewPassword").val();
-	var confirmPassword = $("#txtConfirmPassword").val();
-	
-	if(password == "" && confirmPassword =="") {
-		$("#checkPasswordMatch").html("");
-		$("#updateUserInfoButton").prop('disabled', false);
-	} else {
-		if(password != confirmPassword) {
-			$("#checkPasswordMatch").html("Passwords do not match!");
-			$("#updateUserInfoButton").prop('disabled', true);
-		} else {
-			$("#checkPasswordMatch").html("Passwords match");
-			$("#updateUserInfoButton").prop('disabled', false);
-		}
-	}
-}
-
-$(document).ready(function(){
-	$(".cartItemQty").on('change', function(){
-		var id=this.id;
+$(document).ready(function() {
+	$('.delete-book').on('click', function (){
+		/*<![CDATA[*/
+	    var path = /*[[@{/}]]*/'remove';
+	    /*]]>*/
 		
-		$('#update-item-'+id).css('display', 'inline-block');
+		var id=$(this).attr('id');
+		
+		bootbox.confirm({
+			message: "Are you sure to remove this book? It can't be undone.",
+			buttons: {
+				cancel: {
+					label:'<i class="fa fa-times"></i> Cancel'
+				},
+				confirm: {
+					label:'<i class="fa fa-check"></i> Confirm'
+				}
+			},
+			callback: function(confirmed) {
+				if(confirmed) {
+					$.post(path, {'id':id}, function(res) {
+						location.reload();
+					});
+				}
+			}
+		});
 	});
-	$("#theSameAsShippingAddress").on('click', checkBillingAddress);
-	$("#txtConfirmPassword").keyup(checkPasswordMatch);
-	$("#txtNewPassword").keyup(checkPasswordMatch);
+	
+	var bookIdList=[];
+	
+	#('.checkboxBook').click(function() {
+		var id=$(this).attr('id');
+		if(this.checked){
+			bookIdList.push(id);
+		} else {
+			bookIdList.splice(bookIdList.indexOf(id), 1);
+		}
+	})
+	
+	$('#deleteSelected').click(function() {
+		/*<![CDATA[*/
+	    var path = /*[[@{/}]]*/'removeList';
+	    /*]]>*/
+	    
+	    bootbox.confirm({
+			message: "Are you sure to remove all selected books? It can't be undone.",
+			buttons: {
+				cancel: {
+					label:'<i class="fa fa-times"></i> Cancel'
+				},
+				confirm: {
+					label:'<i class="fa fa-check"></i> Confirm'
+				}
+			},
+			callback: function(confirmed) {
+				if(confirmed) {
+					$.ajax({
+						type: 'POST',
+						url: path,
+						data: JSON.stringify(bookIdList),
+						contentType: "application/json",
+						success: function(res) {console.log(res); location.reload()},
+						error: function(res){console.log(res); location.reload();}
+					});
+				}
+			}
+		});
+	});
+	
+	$("selectAllBooks").click(function() {
+		if($(this).prop("checked")==true) {
+			$(".checkBox").click();
+		} else if ($(this).prop("checked")==false) {
+			$(".checkBox").click();
+		}
+	})
 });
